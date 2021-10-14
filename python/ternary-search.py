c0d26f65ab6ceab53afff129f47c8e39cf468502
ganimtron-10 <praneethshetty10@gmail.com>
2021-10-14 21:57:25 +0530

    [py] adding ternary search

---
+++ python/ternary-search.py
@@ -0,0 +1,46 @@
+import math
+
+def ternarysearch(arr,element,l,h):
+    '''
+    Using Ternary Search to find the element in list.
+    Method: recursive
+
+    Parameter
+    ---------
+    arr: (list)
+    A sorted list on which we are going to apply ternary search
+    
+    element: (any)
+    The element which we are searching for in the list
+
+    l: (int)
+    The lower index of the list
+
+    h: (int)
+    The higher index of the list
+
+    Returns
+    -------
+    (int) Index of element if found else None
+    '''
+    if h >= l:
+        mid1 = l + (h-l) // 3
+        mid2 = h - (h-l) // 3
+
+        if arr[mid1] == element:
+            return mid1
+        if arr[mid2] == element:
+            return mid2
+
+        if arr[mid1] > element:
+            return ternarysearch(arr, element, l, mid1-1)
+        elif arr[mid2] < element:
+            return ternarysearch(arr, element, mid2+1, h)
+        else:
+            return ternarysearch(arr, element, mid1+1, mid2-1)
+
+    return None
+
+if __name__ == "__main__":
+    l = [17, 74, 102, 149, 398, 792, 797, 834, 987]
+    print(ternarysearch(l,1,0,len(l)-1))
